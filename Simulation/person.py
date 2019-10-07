a481369feeaa495919158d43d57a9baa26089c79
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-10-07 15:18:53 -0700

    more person tests

--- Simulation/person.py
+++ Simulation/person.py
@@ -39,31 +39,31 @@ class Person(object):
 ''' These are simple tests to ensure that you are instantiating your Person class correctly. '''
 def test_vacc_person_instantiation():
     # create some people to test if our init method works as expected
-    person = Person(1, True, virus)
+    person = Person(1, True)
     assert person._id == 1
     assert person.is_alive is True
     assert person.is_vaccinated is True
-    assert person.infection is virus
+    assert person.infection is None
 
 
 def test_not_vacc_person_instantiation():
     person = Person(2, False)
-    # TODO: complete your own assert statements that test
-    # the values at each attribute
-    # assert ...
-    pass
 
+    assert person._id == 2
+    assert person.is_alive is True
+    assert person.is_vaccinated is False
+    assert person.infection is None
 
 def test_sick_person_instantiation():
     # Create a Virus object to give a Person object an infection
     virus = Virus("Dysentery", 0.7, 0.2)
     # Create a Person object and give them the virus infection
     person = Person(3, False, virus)
-    # TODO: complete your own assert statements that test
-    # the values at each attribute
-    # assert ...
-    pass
 
+    assert person._id == 3
+    assert person.is_alive is True
+    assert person.is_vaccinated is False
+    assert person.infection is virus
 
 def test_did_survive_infection():
     # TODO: Create a Virus object to give a Person object an infection
