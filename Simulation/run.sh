2ae49dec04ef8c1c15e99611f38376270d4285f5
ninjaaung <ninja.aung02@gmail.com>
2019-10-11 00:18:32 -0700

    Shell Shell Shell

--- Simulation/run.sh
+++ Simulation/run.sh
@@ -1,4 +1,35 @@
-echo "Running Simulation of Ebola"
-echo "100000 0.90 Ebola 0.70 0.25 10"
-python3 simulation.py 100 0.90 Ebola 0.70 0.25 10
+
+python3 simulation.py {population size} {vacc_percentage} {virus_name} {mortality_rate} {repro_rate} {number of people initially infected}
+
+popsize=
+vaccper=
+name=
+mortal=
+repro=
+intial=
+
+
+usage()
+{
+    echo "usage: run [-h help] [-p population size ] [-v vacc_percentage] [-n name] [-m mortality_rate] [-r repro_rate] [-i intially_infected] "
+    exit 1
+}
+
+function cleanUp()
+{
+    echo "\n${red}Error: Script Canceled"
+    exit 2
+}
+read -p "[Population Size]: " popsize
+read -p "[Vaccinated Percentage]: " vaccper
+read -p "[Virus Name]: " name
+read -p "[Mortality Rate]: " mortal
+read -p "[Reproduction Rate]: " repro
+read -p "[Intial infected]" intial
+
+
+
+trap cleanUp 2
+
+python3 simulation.py $popsize $vaccper $name $mortal $repro $intial
 cat *.txt
